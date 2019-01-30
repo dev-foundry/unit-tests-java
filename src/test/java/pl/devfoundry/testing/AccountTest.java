9334f81edfc405152d65b77a734d5f6a6367b7d1
Pawel Cwik <pawel.cwik@sabre.com>
2019-01-30 18:03:55 +0100

    2.6 Given-when-then

--- src/test/java/pl/devfoundry/testing/AccountTest.java
+++ src/test/java/pl/devfoundry/testing/AccountTest.java
@@ -3,14 +3,32 @@ package pl.devfoundry.testing;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AccountTest {
 
 
-    @Test
+    @Test 
     public void myFirstTest() {
+
+        //given
         Account newAccount = new Account();
 
+        //then
         assertFalse(newAccount.isActive(), "Newly created account is not activated");
+    }
+
+    @Test
+    public void mySecondTest() {
+
+        //given
+        Account newAccount = new Account();
+
+        //when
+        newAccount.activate();
+
+        //then
+        assertTrue(newAccount.isActive(), "'active' is set to true on Activated account");
+
     }
 }
