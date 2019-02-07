f65e89c4587d2c0b293eb2e5cd566ddbc7403173
Dawid Nowak <devfoundry01@gmail.com>
2019-02-07 18:44:30 +0100

    3.10 anyOf, allOf and assertAll

--- src/main/java/pl/devfoundry/testing/Cart.java
+++ src/main/java/pl/devfoundry/testing/Cart.java
@@ -7,6 +7,10 @@ class Cart {
 
     private List<Order> orders = new ArrayList<>();
 
+    public List<Order> getOrders() {
+        return orders;
+    }
+
     void addOrderToCart(Order order) {
         this.orders.add(order);
     }
--- src/test/java/pl/devfoundry/testing/CartTest.java
+++ src/test/java/pl/devfoundry/testing/CartTest.java
@@ -5,7 +5,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
 
 @DisplayName("Test cases for Cart")
@@ -22,6 +26,44 @@ class CartTest {
         //when
         //then
         assertTimeout(Duration.ofMillis(100), cart::simulateLargeOrder);
+
+    }
+
+    @Test
+    void cartShouldNotBeEmptyAfterAddingOrderToCart() {
+
+        //given
+        Order order = new Order();
+        Cart cart = new Cart();
+
+        //when
+        cart.addOrderToCart(order);
+
+        //then
+        assertThat(cart.getOrders(), anyOf(
+                notNullValue(),
+                hasSize(1),
+                is(not(empty())),
+                is(not(emptyCollectionOf(Order.class)))
+        ));
+
+        assertThat(cart.getOrders(), allOf(
+                notNullValue(),
+                hasSize(1),
+                is(not(empty())),
+                is(not(emptyCollectionOf(Order.class)))
+        ));
+
+        assertAll("This is a group of assertions for cart",
+                () -> assertThat(cart.getOrders(), notNullValue()),
+                () -> assertThat(cart.getOrders(), hasSize(1)),
+                () -> assertThat(cart.getOrders(), is(not(empty()))),
+                () -> assertThat(cart.getOrders(), is(not(emptyCollectionOf(Order.class)))),
+                () -> {
+                    List<Meal> mealList = cart.getOrders().get(0).getMeals();
+                    assertThat(mealList, empty());
+                }
+        );
 
     }
 
