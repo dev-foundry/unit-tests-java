0306bbf77e3db6bebc934f50dc80ab4f5424a479
Dawid Nowak <devfoundry01@gmail.com>
2019-02-21 18:27:06 +0100

    5.4 Added lambdas method

--- src/test/java/pl/devfoundry/testing/cart/CartServiceTest.java
+++ src/test/java/pl/devfoundry/testing/cart/CartServiceTest.java
@@ -115,22 +115,46 @@ class CartServiceTest {
     }
 
     @Test
+    void processCartShouldSendToPrepareWithLambdas() {
+
+        //given
+        Order order = new Order();
+        Cart cart = new Cart();
+        cart.addOrderToCart(order);
+
+        CartHandler cartHandler = mock(CartHandler.class);
+        CartService cartService = new CartService(cartHandler);
+
+        given(cartHandler.canHandleCart(argThat(c -> c.getOrders().size() > 0))).willReturn(true);
+
+        //when
+        Cart resultCart = cartService.processCart(cart);
+
+        //then
+        then(cartHandler).should().sendToPrepare(cart);
+        assertThat(resultCart.getOrders(), hasSize(1));
+        assertThat(resultCart.getOrders().get(0).getOrderStatus(), equalTo(OrderStatus.PREPARING));
+
+    }
+
+
+    @Test
     void canHandleCartShouldThrowException() {
 
         //given
         Order order = new Order();
         Cart cart = new Cart();
         cart.addOrderToCart(order);
+
         CartHandler cartHandler = mock(CartHandler.class);
         CartService cartService = new CartService(cartHandler);
 
-        given(cartHandler.canHandleCart(any(Cart.class))).willThrow(IllegalStateException.class);
+        given(cartHandler.canHandleCart(cart)).willThrow(IllegalStateException.class);
 
         //when
         //then
         assertThrows(IllegalStateException.class, () -> cartService.processCart(cart));
 
     }
-
 
 }
